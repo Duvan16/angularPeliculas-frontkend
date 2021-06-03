ba74635375ded761963258adaedc496c98ce346f
Duvan <duvan4448@gmail.com>
2021-06-02 21:49:05 -0500

    2-ngClassEstilosDinamicamente

--- src/app/utilidades/rating/rating.component.ts
+++ src/app/utilidades/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
+  @Input()
+  maximoRating = 5;
+  @Input()
+  ratingSeleccionado = 0;
+  @Output()
+  rated: EventEmitter<number> = new EventEmitter<number>();
+  maximoRatingArr: any = [];
+  votado = false;
+  ratingAnterior: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.maximoRatingArr = Array(this.maximoRating).fill(0);
+  }
+
+  manejarMouseEnter(index: number): void {
+    this.ratingSeleccionado = index + 1;
+  }
+
+  manejarMouseLeave() {
+    if (this.ratingAnterior !== 0) {
+      this.ratingSeleccionado = this.ratingAnterior;
+    } else {
+      this.ratingSeleccionado = 0;
+    }
+  }
+
+  rate(index: number): void {
+    this.ratingSeleccionado = index + 1;
+    this.votado = true;
+    this.ratingAnterior = this.ratingSeleccionado;
+    this.rated.emit(this.ratingSeleccionado);
   }
 
 }
