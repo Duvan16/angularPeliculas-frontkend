33452c1b41725e779bdb44a678c4507afa9763f7
Duvan <duvan4448@gmail.com>
2021-06-05 15:44:02 -0500

    5-RuteoyNavegacionJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RatingComponent } from './utilidades/rating/rating.component';
 import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
+import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
+import { IndiceActoresComponent } from './actores/indice-actores/indice-actores.component';
+import { CrearActorComponent } from './actores/crear-actor/crear-actor.component';
+import { IndiceCinesComponent } from './cines/indice-cines/indice-cines.component';
+import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
+import { CrearCineComponent } from './cines/crear-cine/crear-cine.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,12 +25,21 @@ import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
     ListadoGenericoComponent,
     MenuComponent,
     RatingComponent,
-    CicloDeVidaComponent
+    CicloDeVidaComponent,
+    LandingPageComponent,
+    IndiceGenerosComponent,
+    CrearGeneroComponent,
+    IndiceActoresComponent,
+    CrearActorComponent,
+    IndiceCinesComponent,
+    CrearPeliculaComponent,
+    CrearCineComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
