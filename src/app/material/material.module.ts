dd873bc1b1acf81c8f8bf9c58f263795059f0815
Duvan <duvan4448@gmail.com>
2021-06-19 18:32:19 -0500

    9-filtroPeliculas

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,7 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatSelectModule } from '@angular/material/select'
 @NgModule({
   declarations: [],
   exports: [
@@ -13,7 +13,8 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ],
   imports: [
     CommonModule
