daa365490793b6de909be260e64e2991ad6794c9
Duvan <duvan4448@gmail.com>
2021-06-19 17:14:47 -0500

    8-Generos

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule}  from '@angular/material/form-field';
-import {MatInputModule}  from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -12,7 +12,8 @@ import {MatInputModule}  from '@angular/material/input';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   imports: [
     CommonModule
